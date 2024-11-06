d903570e4bac0a3bdb93a0d6412f6017930a0c8e
Changmini <cml039465@gmail.com>
2024-11-06 17:09:09 +0900

    .

---
+++ src/baekjoon/level2839/Main.java
@@ -0,0 +1,35 @@
+package baekjoon.level2839;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int x = (N / 5);
+		int y = (N / 3);
+		int a = 0;
+		int b = 0;
+		boolean res = false;
+		for (int i = x; i >= 0; i--) {
+			for (int j = y; j >= 0; j--) {
+				a = 5 * i;
+				b = 3 * j;
+				res = (N == a + b); 
+				if (res) {
+					x = i;
+					y = j;
+					break;
+				}
+			}
+			if (res) {
+				break;
+			}
+		}
+		System.out.println(res ? x+y : -1);
+	}
+	
+}
