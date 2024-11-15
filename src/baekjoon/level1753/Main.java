5b05cf6994b191dc56a952a7033483fed410fb49
Changmini <cml039465@gmail.com>
2024-11-15 19:35:04 +0900

    이해

--- src/baekjoon/level1753/Main.java
+++ src/baekjoon/level1753/Main.java
@@ -3,13 +3,12 @@ package baekjoon.level1753;
 import java.io.*;
 import java.util.*;
 
-// 노드 class
 class Node {
-    int end;
+    int vertex;
     int weight;
 
-    public Node(int end, int weight) {
-        this.end = end;
+    public Node(int vertex, int weight) {
+        this.vertex = vertex;
         this.weight = weight;
     }
 }
@@ -23,52 +22,48 @@ public class Main {
         int E = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(br.readLine());
 
-        boolean[] visit = new boolean[V+1];     // 방문 처리 배열
-        int[] result = new int[V+1];            // 최단 경로 값 저장 배열
-        List<Node>[] list = new List[V+1];      // 연결 정보 저장 배열
+        boolean[] visit = new boolean[V+1];
+        int[] weight = new int[V+1];    
+        ArrayList<Node>[] list = new ArrayList[V+1];
 		
-        // 연결 정보 저장할 배열, 최단 경로 값 저장 배열 초기화
         for(int i = 1; i<=V; i++) {
             list[i] = new ArrayList<>();
-            result[i] = Integer.MAX_VALUE;
+            weight[i] = Integer.MAX_VALUE;
         }
 
         for(int i = 0; i<E; i++) {
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());       // 출발
-            int v = Integer.parseInt(st.nextToken());       // 도착
-            int w = Integer.parseInt(st.nextToken());       // 가중치
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
 
             list[u].add(new Node(v, w));
         }
 
-        // 다익스트라
         PriorityQueue<Node> queue = new PriorityQueue<>((o1, o2) -> o1.weight - o2.weight);
-        result[K] = 0;
+        weight[K] = 0;
         queue.add(new Node(K, 0));
-
         while(!queue.isEmpty()) {
-            Node now = queue.poll();		// 현재 방문 정점
-            if(!visit[now.end]) visit[now.end] = true;		// 방문처리
-            // 현재 정정과 연결된 간선들에 대해 판단
-            for(int i = 0; i<list[now.end].size(); i++) {
-            	// 현재 정점에서 이어질 다음 정점
-                Node next = list[now.end].get(i);
-                // 다음 정점이 방문하지 않았고, 
-                // 현재 가중치 + 해당 정점으로 향하는 가중치 값 < 해당 정점으로의 최단 경로 값이라면
-                if(!visit[next.end] && now.weight + next.weight < result[next.end]) {
-                    // 해당 정점으로의 최단 경로 값 Update
-                    result[next.end] = now.weight + next.weight;
-                    // 다음 방문할 예정이므로 queue에 넣어주기
-                    queue.add(new Node(next.end, result[next.end]));
+            Node now = queue.poll();
+            if(!visit[now.vertex]) 
+            	visit[now.vertex] = true;
+            int size = list[now.vertex].size();
+            for(int i = 0; i<size; i++) {
+                Node next = list[now.vertex].get(i);
+                int sumW = now.weight + next.weight;
+                if(!visit[next.vertex] && sumW < weight[next.vertex]) {
+                	weight[next.vertex] = sumW;
+                    queue.add(new Node(next.vertex, weight[next.vertex]));
                 }
             }
         }
 		
-        // 출력
+        StringBuilder sb = new StringBuilder();
         for(int i = 1; i<=V; i++) {
-            if(result[i] == Integer.MAX_VALUE) System.out.println("INF");
-            else System.out.println(result[i]);
+            if(weight[i] == Integer.MAX_VALUE) 
+            	sb.append("INF").append("\n");
+            else sb.append(weight[i]).append("\n");
         }
+        System.out.println(sb.toString());
     }
 }
